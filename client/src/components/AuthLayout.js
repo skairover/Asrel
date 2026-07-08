1de4a04a882f7d51bac2cf90310d6102e54ca56a
skairover <sofianehot01@gmail.com>
2026-07-08 08:50:57 +0100

    removing images

--- client/src/components/AuthLayout.js
+++ client/src/components/AuthLayout.js
@@ -1,7 +1,4 @@
-import Image from "next/image";
-import notebook from "/closed-notebook.png";
-import ebook from "/E-ink-tablet.png";
-import openedbook from "/opened-book-txt-img.png";
+
 import { useEffect, useState } from "react";
 
 export default function AuthLayout({ children }) {
@@ -21,19 +18,7 @@ export default function AuthLayout({ children }) {
   return (
     <div className="max-w-screen h-screen flex bg-[#F5EFE6] relative overflow-y-hidden">
       {/* Decorative floating images - shared for all devices */}
-      {images.map((img, i) => (
-        <div
-          key={i}
-          className="absolute w-20 sm:w-24 md:w-28 opacity-50 "
-          style={{
-            top: positions[i]?.top,
-            left: positions[i]?.left,
-          }}
-        >
-          <Image src={img} alt="book icon"  width={100}
-  height={100}/>
-        </div>
-      ))}
+
 
       {/* Left side: form */}
       <div className="z-10 w-full h-full md:w-1/2 flex flex-col justify-center items-center p-6">
